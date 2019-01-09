e6031d2225dfc627236c34b19d7511f9bce7cd03
Jesus <jesus.jg.medina@gmail.com>
2019-01-09 12:41:27 -0500

    Manual submit

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name
+    @name = "Fido"
+  end
+
+  def name=(namer)
+    @name = namer
+  end
+  def breed
+    @breed = "Beagle"
+  end
+  def breed=(breeder)
+    @breed = breeder
+  end
+end
