88e71c4af2ca3338583664689ce452646809ebb9
Chris Kakos <ckakos@gmail.com>
2019-01-10 16:23:09 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+  #setter
+  def name=(name)
+    @name = name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  #getter
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
+
+end
